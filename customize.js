562927b52beae27284ddb98096d32036b33e69bd
mooncryption <33406076+mooncryption@users.noreply.github.com>
2017-11-12 14:23:31 -0800

    Update customize.js

--- customize.js
+++ customize.js
@@ -1,24 +1,22 @@
-
-
 function compilewidget() {
     var choiceWidth = parseFloat(document.getElementById("choicewidth").value);
     var choiceHeight = parseFloat(document.getElementById("choiceheight").value);
     var prechoiceBG = !(document.getElementById("choicenobg").checked);
     var choiceBG = "false"; 
-    if (prechoiceBG) {
+    if (prechoiceBG == true) {
         choiceBG = "true";
     }
     var prechoiceBorder = !(document.getElementById("choicenoborder").checked);
     var choiceBorder = "false";
-    if (prechoiceBorder) {
+    if (prechoiceBorder == true) {
         choiceBorder = "true";
     }
     var choiceBase = "USD";
-    if ((document.getElementById("choiceusebtc").checked)) {
+    if ((document.getElementById("choiceusebtc").checked) == true) {
         choiceBase = "BTC";
     }
     var params = "background=" + choiceBG + "&border=" + choiceBorder + "&base=" + choiceBase;
-    var code = "<iframe src=\"https://mooncryption.github.io/steem-price-widget/widget.html?" + params + "\"width=\"" + choiceWidth + "\" height=\"" + choiceHeight + "\" frameBorder=\"0\"></iframe>";
+    var code = "<iframe src=\"https://mooncryption.github.io/steem-price-widget/widget.html?" + params + "\" width=\"" + choiceWidth + "\" height=\"" + choiceHeight + "\" frameBorder=\"0\"></iframe>";
     console.log("USING CODE: " + code);
     document.getElementById("widgetsample").innerHTML = code;
 }
