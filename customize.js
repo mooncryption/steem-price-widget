4db6496bc2f62dcd3fbb74b9f9cccdd402253858
mooncryption <33406076+mooncryption@users.noreply.github.com>
2017-11-12 14:21:00 -0800

    Update customize.js

--- customize.js
+++ customize.js
@@ -3,13 +3,21 @@
 function compilewidget() {
     var choiceWidth = parseFloat(document.getElementById("choicewidth").value);
     var choiceHeight = parseFloat(document.getElementById("choiceheight").value);
-    var choiceBG = !(document.getElementById("choicenobg").checked);
-    var choiceBorder = !(document.getElementById("choicenoborder").checked);
+    var prechoiceBG = !(document.getElementById("choicenobg").checked);
+    var choiceBG = "false"; 
+    if (prechoiceBG) {
+        choiceBG = "true";
+    }
+    var prechoiceBorder = !(document.getElementById("choicenoborder").checked);
+    var choiceBorder = "false";
+    if (prechoiceBorder) {
+        choiceBorder = "true";
+    }
     var choiceBase = "USD";
     if ((document.getElementById("choiceusebtc").checked)) {
         choiceBase = "BTC";
     }
-    var params = "background=" + toString(choiceBG) + "&border=" + toString(choiceBorder) + "&base=" + toString(choiceBase);
+    var params = "background=" + choiceBG + "&border=" + choiceBorder + "&base=" + choiceBase;
     var code = "<iframe src=\"https://mooncryption.github.io/steem-price-widget/widget.html?" + params + "\"width=\"" + choiceWidth + "\" height=\"" + choiceHeight + "\" frameBorder=\"0\"></iframe>";
     console.log("USING CODE: " + code);
     document.getElementById("widgetsample").innerHTML = code;
